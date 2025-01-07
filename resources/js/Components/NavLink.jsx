5a3864686d34874b9e1a209408e6fafb9242911f
Karim Omrane <karimomrane7@gmail.com>
2025-01-08 00:48:35 +0100

    Animations

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -1,23 +1,36 @@
 import { Link } from '@inertiajs/react';
+import { motion } from 'framer-motion';
 
 export default function NavLink({
     active = false,
     className = '',
     children,
     ...props
-}) {
+})
+{
     return (
         <Link
             {...props}
             className={
-                'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
+                'relative inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
                 (active
-                    ? 'border-indigo-400 text-gray-900 focus:border-indigo-700 dark:border-indigo-600 dark:text-gray-100'
-                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-gray-300 dark:focus:border-gray-700 dark:focus:text-gray-300') +
+                    ? 'text-gray-900 focus:text-gray-900 dark:text-gray-100'
+                    : 'text-gray-500 hover:text-gray-700 focus:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 dark:focus:text-gray-300') +
                 className
             }
         >
             {children}
+            {/* Bottom border animation */}
+            {active && (
+                <motion.div
+                    layoutId="underline"
+                    className="absolute bottom-0 left-0 h-[2px] w-full bg-indigo-400 dark:bg-indigo-600"
+                    initial={{ width: 0 }}
+                    animate={{ width: '100%' }}
+                    exit={{ width: 0 }}
+                    transition={{ duration: 0.3 }}
+                />
+            )}
         </Link>
     );
 }
