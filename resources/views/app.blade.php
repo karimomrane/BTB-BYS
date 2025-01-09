2d8fc541cd787eb3a0339ad3e1b8b8cd38c0c552
Karim Omrane <karimomrane7@gmail.com>
2025-01-09 15:27:22 +0100

    fix icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" href="/logo.png" type="image/ico">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
